314e7bf57681cb8c1f3c72e52b41c27814e328ae
Patrick753951 <2970539385wqc@gmail.com>
2024-08-07 22:37:58 +0800

    Scrapy fist demo finish

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-Init
-创建爬虫：scrapy generate 爬虫名称 允许爬虫的域名
-运行爬虫：scrapy crawl 爬虫名称
+命令介绍
+----
+- 创建爬虫
+
+scrapy generate 爬虫名称 允许爬虫的域名
+
+- 运行爬虫：
+
+scrapy crawl 爬虫名称
+scrapy crawl 爬虫名称 --nolog
--- ScrapyDemo/ScrapyDemo/pipelines.py
+++ ScrapyDemo/ScrapyDemo/pipelines.py
@@ -2,12 +2,22 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
+import pandas as pd
+from pathlib import Path
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
 
 class ScrapydemoPipeline:
     def process_item(self, item, spider):
+        # 写入文件
+        # print(item)
+        # 字典数据写入
+        df = pd.DataFrame(item, index=[0, 1, 2])
+        file_path = "../outputs/teachers.cvs"
+        path = Path(file_path)
+        if not path.exists() or False:
+            df.to_csv(file_path, mode='a', header=True, index=False)
+        else:
+            df.to_csv(file_path, mode='a', header=False, index=False)
         return item
